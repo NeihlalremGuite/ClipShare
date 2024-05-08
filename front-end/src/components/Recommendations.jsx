432d49c64ee76d51f210ca6a5151ee301f7622d6
Neihlalrem Guite <neihlalremguite@Pro.local>
2024-05-08 15:56:00 +0530

    clipshare

---
+++ front-end/src/components/Recommendations.jsx
@@ -0,0 +1,34 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import Card from "./Card";
+import { useSelector } from "react-redux";
+
+const Container = styled.div`
+  flex: 2;
+`;
+
+const Recommendations = ({ tags }) => {
+  const [videos, setVideos] = useState([]);
+  const { currentVideo} = useSelector((state) => state.video);
+
+  useEffect(() => {
+    const fetchVideos = async () => {
+      const res = await axios.get(`/videos/tags?tags=${tags}`);
+      setVideos(res.data);
+    };
+    fetchVideos();
+  }, [tags]);
+
+  return (
+    <Container>
+      {videos.map((video) =>
+        video._id !== currentVideo._id && (
+          <Card type="sm" key={video._id} video={video} />
+        )
+      )}
+    </Container>
+  );
+};
+
+export default Recommendations;
